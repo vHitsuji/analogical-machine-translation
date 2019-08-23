d56e869cbe65b4bb7949c7721875ad80a98ecd36
Valentin Taillandier <49441635+vHitsuji@users.noreply.github.com>
2019-08-23 15:07:26 +0900

    Delete matrixScript.sh

--- matrixScript.sh
+++
@@ -1,8 +0,0 @@
-python3 makedataset.py \
---padding \
---input ../corpora/en-fr.cuboid.nlg \
---output en-fr.matrices \
---first_language_model ../model/wordvectors/FastText/en.kv \
---second_language_model ../model/wordvectors/FastText/fr.kv \
---bilingual_model ../model/bilingual/en_fr_symetric.model 
-
